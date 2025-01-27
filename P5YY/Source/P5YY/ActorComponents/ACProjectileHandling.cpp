e8a2ccb46131befd19158f1b48805693c3a96444
burlin_gauna <burlin_gauna@yahoo.com>
2025-01-27 12:51:07 +0800

    added new class for projectile component and data

---
+++ P5YY/Source/P5YY/ActorComponents/ACProjectileHandling.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ACProjectileHandling.h"
+
+
+// Sets default values for this component's properties
+UACProjectileHandling::UACProjectileHandling()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UACProjectileHandling::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UACProjectileHandling::TickComponent(float DeltaTime, ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ P5YY/Source/P5YY/Structs/ProjectileData.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "ProjectileData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FProjectileData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int ProjetileId;
+
+	
+	FProjectileData()
+	{
+	}
+};
