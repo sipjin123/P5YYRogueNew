d3a6ca32a1e84422cbbf7739a606af190b589332
burlin_gauna <burlin_gauna@yahoo.com>
2025-02-03 14:17:56 +0800

    added new buff/debuff class enum

---
+++ P5YY/Source/P5YY/Enums/BuffType.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EBuffType : uint8 {
+	None = 0 UMETA(DisplayName = "None"),
+	SpeedBoost = 1 UMETA(DisplayName = "SpeedBoost"),
+	DefenseBoost = 2 UMETA(DisplayName = "DefenseBoost"),
+	DodgeBoost = 3 UMETA(DisplayName = "DodgeBoost"),
+	AttackBoost = 4 UMETA(DisplayName = "AttackBoost"),
+	AttackSpeedBoost = 5 UMETA(DisplayName = "AttackSpeedBoost"),
+};
+
+struct BuffType
+{
+	
+};
---
+++ P5YY/Source/P5YY/Enums/DebuffType.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+UENUM(BlueprintType)
+enum class EDebuffType : uint8 {
+	None = 0 UMETA(DisplayName = "None"),
+	Stun = 1 UMETA(DisplayName = "Stun"),
+	Slow = 2 UMETA(DisplayName = "Slow"),
+	Poison = 3 UMETA(DisplayName = "Poison"),
+	Freeze = 4 UMETA(DisplayName = "Freeze"),
+	Burn = 5 UMETA(DisplayName = "Burn"),
+	Bleed = 6 UMETA(DisplayName = "Bleed"),
+};
+
+struct DebuffType
+{
+	
+};
