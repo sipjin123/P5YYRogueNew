f96cd569d12ffe7270325199e021be54c20ca4f5
burlin_gauna <burlin_gauna@yahoo.com>
2025-02-15 15:02:38 +0800

    added new subjects to be used by blueprints to add listeners and observer bindings

---
+++ P5YY/Source/P5YY/Utility/Subject.cpp
@@ -0,0 +1,24 @@
+﻿#include "Subject.h"
+#include "Observer.h"
+
+void USubject::AddObserver(TScriptInterface<IObserver> Observer)
+{
+		UE_LOG(LogTemp, Warning, TEXT("Observer was Added"));
+	Observers.Add((IObserver*)Observer.GetInterface());
+}
+
+void USubject::RemoveObserver(TScriptInterface<IObserver> Observer)
+{
+		UE_LOG(LogTemp, Warning, TEXT("Observer was Removed"));
+	Observers.Remove((IObserver*)Observer.GetInterface());
+}
+
+void USubject::Notify(UObject* Entity, EStatEvent Event)
+{
+	UE_LOG(LogTemp, Warning, TEXT("NOTIFY"));
+	for (auto Observer : Observers) {
+		
+		UE_LOG(LogTemp, Warning, TEXT("NOTIFY Observer"));
+		Observer->OnNotify(Entity, Event);
+	}
+}
---
+++ P5YY/Source/P5YY/Utility/Subject.h
@@ -0,0 +1,31 @@
+﻿
+#pragma once
+
+#include "Containers/Array.h"
+#include "P5YY/Enums/StatEvent.h"
+#include "Subject.generated.h"
+
+class IObserver;
+
+UCLASS(BlueprintType)
+class P5YY_API USubject : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Observer Pattern")
+	void AddObserver(TScriptInterface<IObserver> Observer);
+
+	UFUNCTION(BlueprintCallable, Category = "Observer Pattern")
+	void RemoveObserver(TScriptInterface<IObserver> Observer);
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Observer Pattern")
+	void Notify(UObject* Entity, EStatEvent Event);
+
+private:
+	// UINTERFACES generate their destructor private,
+	// so shared pointer can't access it to manage its life cycle,
+	// thus giving us an error at compile time if we try to use them.
+	TArray<IObserver*> Observers;
+};
