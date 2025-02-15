ed82a7942a0a149c2b95aa6520c4c3e9497318df
burlin_gauna <burlin_gauna@yahoo.com>
2025-02-15 15:02:11 +0800

    updated observer class to allow notification triggers for listeners

---
+++ P5YY/Source/P5YY/Utility/Observer.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "P5YY/Enums/StatEvent.h"
+#include "Observer.generated.h"
+
+// UE4 interface.
+// More info at: https://docs.unrealengine.com/latest/INT/Programming/UnrealArchitecture/Reference/Interfaces/
+
+// This class does not need to be modified.
+UINTERFACE(BlueprintType)
+class UObserver : public UInterface
+{
+	GENERATED_BODY()
+};
+
+// Add interface functions to this class. 
+// This is the class that will be inherited to implement this interface.
+class P5YY_API IObserver
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnNotify(UObject* Entity, EStatEvent Event) = 0;
+};
+
+
+
+
+
+// OBSOLETE
+/*
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Observer.generated.h"
+
+DECLARE_MULTICAST_DELEGATE(FSomethingChanged);
+
+UCLASS()
+class P5YY_API UObserver : public UObject
+{
+	GENERATED_BODY()
+public:
+	UObserver();
+	
+	FSomethingChanged OnSomethingChanged;
+
+	void ChangeSomething()
+	{
+		OnSomethingChanged.Broadcast();
+	}
+};*/
