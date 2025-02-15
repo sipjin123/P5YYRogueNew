e0dc69e32b38b4170171a2e2ba2a8621e656f2fa
burlin_gauna <burlin_gauna@yahoo.com>
2025-02-15 15:03:00 +0800

    created stat observer manager to allow assignment and handling of observables

---
+++ P5YY/Source/P5YY/Utility/StatObserverManager.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+// Learn more from :https://github.com/ChusMM/CppDesingPatterns/blob/master/Source/CppDesignPatterns/AchievementManager.cpp
+
+#include "StatObserverManager.h"
+
+#include "StatNotification.h"
+
+bool UStatObserverManager::Instantiated_ = false;
+
+UStatObserverManager::UStatObserverManager()
+{
+	// Check if the class is being created as a CDO (i.e., at the editor startup).
+	// If that's the case, we allow its creation without affecting the only-one-instance behaviour.
+	if (!HasAnyFlags(RF_ClassDefaultObject)) {
+		//verifyf(!Instantiated_, TEXT("Trying to instance more than one class of UStatObserverManager"));
+		Instantiated_ = true;
+	}
+
+	StatEventCounter.Add(EStatEvent::EVENT_ON_ASSIGNED, 0);
+	StatEventCounter.Add(EStatEvent::EVENT_ON_UPDATED, 0);
+	StatEventCounter.Add(EStatEvent::EVENT_DEPLETED, 0);
+}
+
+UStatObserverManager::~UStatObserverManager()
+{
+	Instantiated_ = false;
+}
+
+void UStatObserverManager::OnNotify(UObject* Entity, EStatEvent Event)
+{
+	UE_LOG(LogTemp, Warning, TEXT("NOTIFY GENERIC"));
+	switch (Event) {
+	case EStatEvent::EVENT_ON_ASSIGNED:
+		IncreaseEventCounter(Event);
+		UE_LOG(LogTemp, Warning, TEXT("Assigned"));
+		if (StatEventCounter[Event] == 5) {
+			Unlock(EStatEvent::EVENT_ON_ASSIGNED);
+		}
+		break;
+	case EStatEvent::EVENT_ON_UPDATED:
+		IncreaseEventCounter(Event);
+		UE_LOG(LogTemp, Warning, TEXT("Updated"));
+		if (StatEventCounter[Event] == 5) {
+			Unlock(EStatEvent::EVENT_ON_UPDATED);
+		}
+		break;
+	case EStatEvent::EVENT_DEPLETED:
+		IncreaseEventCounter(Event);
+		UE_LOG(LogTemp, Warning, TEXT("Depleted"));
+
+		if (StatEventCounter[Event] == 5) {
+			Unlock(EStatEvent::EVENT_DEPLETED);
+		}
+		break;
+	}
+}
+
+void UStatObserverManager::IncreaseEventCounter(EStatEvent Event)
+{
+	// We need to keep a counter of the number of events that have arriven,
+	// in order to unlock the appropriate achievements.
+	// But we do not need to increase EVERY event; just the ones related to
+	// the achievements.
+	StatEventCounter.Add(Event, ++StatEventCounter[Event]);
+}
+
+void UStatObserverManager::Unlock(EStatEvent StatNotifType)
+{
+	// Save the achievement by its type, just in case we want to do something
+	// with it in the future
+	StatNotification CurrentStatNotif = StatNotification(StatNotifType);
+	StatNotifications.Add(StatNotifType, CurrentStatNotif);
+
+	// Run the achievement custom behaviour
+	CurrentStatNotif.RunBehaviour();
+}
---
+++ P5YY/Source/P5YY/Utility/StatObserverManager.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Observer.h"
+#include "StatNotification.h"
+#include "StatObserverManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P5YY_API UStatObserverManager : public UObject, public IObserver
+{
+	GENERATED_BODY()
+
+public:
+	UStatObserverManager();
+	~UStatObserverManager();
+
+	void OnNotify(UObject* Entity, EStatEvent Event) override;
+
+private:
+	void IncreaseEventCounter(EStatEvent Event);
+	void Unlock(EStatEvent StatNotifType);
+
+private:
+	TMap<EStatEvent, uint32> StatEventCounter;
+	TMap<EStatEvent, StatNotification> StatNotifications;
+
+	static bool Instantiated_;
+};
