dd14bc4121c4d0f02d97784af6d08d850279fa23
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-25 12:41:30 +0800

    added constant class for referencing

---
+++ P5YY/Source/P5YY/Core/RogueConstants.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "RogueConstants.generated.h"
+
+UCLASS()
+class P5YY_API URogueConstants : public UObject
+{
+	GENERATED_BODY()
+public:
+	/** The constant string stored in the header file */
+	inline static const FString MyConstantString = TEXT("Hello from header file!");
+	
+	/** Returns the constant string for Blueprint access */
+	UFUNCTION(BlueprintPure, Category = "Constants")
+	static FString GetMyConstantString() { return MyConstantString; }
+	
+	/** Returns the constant string for Blueprint access */
+	UFUNCTION(BlueprintPure, Category = "Constants")
+	static FString GetMyShorterConstantString() { return TEXT("Hello from header file!"); }
+};
