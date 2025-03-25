d18bd1bc4e9e549a1f50cf300ab623a85dfbb815
burlin_gauna <burlin_gauna@yahoo.com>
2025-03-25 10:21:57 +0800

    created event manager singleton for blueprint library access

---
+++ P5YY/Source/P5YY/Core/RogueEventManager.cpp
@@ -0,0 +1,21 @@
+﻿#include "RogueEventManager.h"
+
+// Need to declare this otherwise the linker cant find where the instance actually exist
+URogueEventManager* URogueEventManager::Instance = nullptr;
+URogueEventManager* URogueEventManager::Get()
+{
+	//static URogueEventManager* Instance = NewObject<URogueEventManager>();
+	//return Instance;
+
+	if (!Instance)
+	{
+		Instance = NewObject<URogueEventManager>();
+		Instance->AddToRoot(); // Prevents garbage collection
+	}
+	return Instance;
+}
+
+void URogueEventManager::SingletonChecker()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Singleton Function Called!"));
+}
---
+++ P5YY/Source/P5YY/Core/RogueEventManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "RogueEventManager.generated.h"
+
+
+// Declare a delegate with two parameters
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnPrintDevText, FString, Message, EDebugMsgType, MessageType);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnPrintWorldText, FString, Message, EDebugColor, ColorType, FVector, Location);
+
+UCLASS(Blueprintable)
+class URogueEventManager : public UObject
+{
+public:
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnPrintDevText OnPrintDevText;
+	UPROPERTY(BlueprintAssignable)
+	FOnPrintWorldText OnPrintWorldText;
+
+	UFUNCTION(BlueprintCallable)
+	static void SingletonChecker();
+	UFUNCTION(BlueprintCallable)
+	static URogueEventManager* Get(); // Singleton accessor
+	
+private:
+	// Internal instance
+	static URogueEventManager* Instance;
+};
