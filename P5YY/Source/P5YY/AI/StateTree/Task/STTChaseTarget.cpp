5a089942cd78994f364d4f1dab1134c38dbbecb3
dhenzzerrudo <dhenzzerrudo@gmail.com>
2025-03-31 15:49:53 +0800

    implement cpp version of chase target

---
+++ P5YY/Source/P5YY/AI/StateTree/Task/STTChaseTarget.cpp
@@ -0,0 +1,100 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STTChaseTarget.h"
+#include "AIController.h"
+#include "Navigation/PathFollowingComponent.h"
+
+float USTTChaseTarget::GetDistanceFromTarget()
+{
+	if(!IsValid(TargetActor))
+	{
+		return 0;
+	}
+
+	DistanceToTarget = FVector::Distance(TargetActor->GetActorLocation(), OwnerActor->GetActorLocation());
+
+	return DistanceToTarget;
+}
+
+void USTTChaseTarget::GetDistanceFromOrigin()
+{
+	if(!IsValid(TargetActor))
+	{
+		return;
+	}
+
+	if(ReturningToPatrol)
+	{
+		return;
+	}
+
+	float distance = FVector::Distance(CastedActor->SpawnPosition, TargetActor->GetActorLocation());
+	if(distance > MaxChaseDistance)
+	{
+		ReturnToPatrol();
+	}
+}
+
+void USTTChaseTarget::ReturnToPatrol()
+{
+	ReturningToPatrol = true;
+	OwnerController->StopMovement();
+	OwnerController->MoveToLocation(CastedActor->SpawnPosition, 5.0, false);
+	OwnerController->ReceiveMoveCompleted.AddDynamic(this, &USTTChaseTarget::OnReturnToPatrolCompleted);
+}
+
+void USTTChaseTarget::OnReturnToPatrolCompleted(FAIRequestID RequestId, EPathFollowingResult::Type RequestStatus)
+{
+	StateTree->SendStateTreeEvent(ReturnToPatrolTag);
+	UE_LOG(LogTemp, Log, TEXT("CHASE COMPLETED"));
+	FinishTask(false);
+}
+
+void USTTChaseTarget::OnChaseCompleted(FAIRequestID RequestId, EPathFollowingResult::Type RequestStatus)
+{
+	GetDistanceFromTarget();
+	if(RequestStatus == EPathFollowingResult::Success)
+	{
+		FinishTask();
+	}
+	else
+	{
+		FinishTask(false);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("CHASE COMPLETED"));
+}
+
+EStateTreeRunStatus USTTChaseTarget::EnterState(FStateTreeExecutionContext& Context,
+                                                const FStateTreeTransitionResult& Transition)
+{
+	UE_LOG(LogTemp, Log, TEXT("ENTERED CHASE"));
+	
+	float distance = GetDistanceFromTarget();
+	if(distance < StrafeRadius)
+	{
+		UE_LOG(LogTemp, Log, TEXT("CHASE COMPLETED"));
+		FinishTask();
+	}
+
+	ReturningToPatrol = false;
+	CastedActor = Cast<AAAICharacter>(OwnerActor);
+	OwnerController = Cast<AAIController>(CastedActor->Controller);
+
+	if(!IsValid(CastedActor))
+	{
+		UE_LOG(LogTemp, Log, TEXT("CHASE failed"));
+		FinishTask(false);
+	}
+
+	GetDistanceFromOrigin();
+
+	if(!ReturningToPatrol)
+	{
+		OwnerController->MoveToActor(TargetActor, AttackRadius * 0.5, false);
+		OwnerController->ReceiveMoveCompleted.AddDynamic(this, &USTTChaseTarget::OnChaseCompleted);
+	}
+	
+	return EStateTreeRunStatus::Running;
+}
---
+++ P5YY/Source/P5YY/AI/StateTree/Task/STTChaseTarget.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Blueprint/StateTreeTaskBlueprintBase.h"
+#include "Components/StateTreeComponent.h"
+#include "P5YY/AI/AAICharacter.h"
+#include "STTChaseTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P5YY_API USTTChaseTarget : public UStateTreeTaskBlueprintBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Context")
+	TObjectPtr<AActor> OwnerActor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	TObjectPtr<AActor> TargetActor;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	float AttackRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	float MaxChaseDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	float StrafeRadius;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Output")
+	float DistanceToTarget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Output")
+	bool ReturningToPatrol;
+
+	UPROPERTY(EditAnywhere)
+	FGameplayTag ReturnToPatrolTag;
+
+protected:
+	AAAICharacter* CastedActor;
+	AAIController* OwnerController;
+	UStateTreeComponent* StateTree;
+	
+	float GetDistanceFromTarget();
+	void GetDistanceFromOrigin();
+
+	void ReturnToPatrol();
+
+	UFUNCTION()
+	void OnReturnToPatrolCompleted(FAIRequestID RequestId, EPathFollowingResult::Type RequestStatus);
+	UFUNCTION()
+	void OnChaseCompleted(FAIRequestID RequestId, EPathFollowingResult::Type RequestStatus);
+
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context, const FStateTreeTransitionResult& Transition) override;
+};
